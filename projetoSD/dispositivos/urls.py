362fb4299dd45357c12d4ced058054aed5e6635b
Jarvis-01 <jefferson.ferreira@ufvjm.edu.br>
2023-10-26 17:26:44 -0300

    organizando rotas

--- projetoSD/dispositivos/urls.py
+++ projetoSD/dispositivos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index1'),
     path('adicionar/', views.adicionar, name='adicionar'),
     path('editar/<int:id>', views.editar, name='editar'),
     path('deletar/<int:id>', views.deletar, name='deletar'),
--- projetoSD/falhas/urls.py
+++ projetoSD/falhas/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index2'),
     path("detalhe/<int:pk>/", views.detalhe, name='detalhe'),
-    path('adicionar/', views.adicionar, name='adicionar'),
+    path('adiciona/', views.adicionar, name='adiciona'),
     path('editar/<int:id>', views.editar, name='editar'),
     path('deletar/<int:id>', views.deletar, name='deletar'),   
 ]
